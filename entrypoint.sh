8ace57565c32ecfb2aa30bca0c2453ea113bf6a0
Rizel Scarlett <blackgirlbytes@github.com>
2022-02-09 11:31:14 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,12 +20,12 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
 set echo off
-mysql -u admin_rizel -p --database=employees --host=demo-actions-db.cbou4ufhnwpk.us-east-1.rds.amazonaws.com --port=3306 --batch -e "select * from people " | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > "$TMP_DIR/$database.sql"
-tar -cpzf "$TMP_DIR/$database-$FILENAME" "$TMP_DIR/$database.sql"
+mysql -u admin_rizel -p --database=employees --host=demo-actions-db.cbou4ufhnwpk.us-east-1.rds.amazonaws.com --port=3306 --batch -e "select * from people " | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > "backup.sql"
+tar -cpzf "$TMP_DIR/$database-$FILENAME" "$backup.sql"
 set echo on
 
 printf "Uploading to S3...\n"
-aws s3 cp "$TMP_DIR/$database-$FILENAME" "s3://$S3_BUCKET_NAME/$S3_FOLDER/$database-$FILENAME"
+aws s3 cp "backup.sql" "s3://$S3_BUCKET_NAME/$S3_FOLDER/backup.sql"
 printf "Uploaded to S3.\n"
 
 printf "Cleaning up...\n"
