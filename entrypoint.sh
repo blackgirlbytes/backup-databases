f85378d69e0bf29dfdd5255029109eec17cfea45
Rizel Scarlett <blackgirlbytes@github.com>
2022-02-09 11:27:17 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,43 +16,18 @@ for line in sys.stdin:
 END
 )
 
-# Convert ENV array into bash array
-IGNORED_DATABASES=$(echo $IGNORED_DATABASES | python -c "$REPLACE")
-DATABASES=$(echo $DATABASES | python -c "$REPLACE")
-
-IGNORED_DATABASE_NAMES=($IGNORED_DATABASES)
-DATABASE_NAMES=($DATABASES)
-
-if [[ -z $DATABASES ]]; then
-  DATABASE_NAMES=()
-fi
-
-DATABASES_TOTAL=${#DATABASE_NAMES[@]}
-
 aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 
-if [[ $DATABASES_TOTAL -eq 0 ]]; then
-  DATABASE_NAMES=$(echo "show databases;" | mysql -h "$DB_HOST" -p"$DB_PASSWORD" -u "$DB_USER")
-fi
+set echo off
+mysql -u admin_rizel -p --database=employees --host=demo-actions-db.cbou4ufhnwpk.us-east-1.rds.amazonaws.com --port=3306 --batch -e "select * from people " | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > "$TMP_DIR/$database.sql"
+tar -cpzf "$TMP_DIR/$database-$FILENAME" "$TMP_DIR/$database.sql"
+set echo on
 
-for database in ${DATABASE_NAMES[@]}; do
-  if [[ " ${IGNORED_DATABASE_NAMES[@]} " =~ " ${database} " ]]; then
-    printf "$database ignored\n"
-  else
-    printf "Backing up $database\n"
+printf "Uploading to S3...\n"
+aws s3 cp "$TMP_DIR/$database-$FILENAME" "s3://$S3_BUCKET_NAME/$S3_FOLDER/$database-$FILENAME"
+printf "Uploaded to S3.\n"
 
-    set echo off
-    mysql -u admin_rizel -p --database=employees --host=demo-actions-db.cbou4ufhnwpk.us-east-1.rds.amazonaws.com --port=3306 --batch -e "select * from people " | sed 's/\t/","/g;s/^/"/;s/$/"/;s/\n//g' > "$TMP_DIR/$database.sql"
-    tar -cpzf "$TMP_DIR/$database-$FILENAME" "$TMP_DIR/$database.sql"
-    set echo on
-
-    printf "Uploading to S3...\n"
-    aws s3 cp "$TMP_DIR/$database-$FILENAME" "s3://$S3_BUCKET_NAME/$S3_FOLDER/$database-$FILENAME"
-    printf "Uploaded to S3.\n"
-
-    printf "Cleaning up...\n"
-    rm -rf "$TMP_DIR/$database-$FILENAME"
-    printf "Cleaned up.\n"
-  fi
-done
+printf "Cleaning up...\n"
+rm -rf "$TMP_DIR/$database-$FILENAME"
+printf "Cleaned up.\n"
